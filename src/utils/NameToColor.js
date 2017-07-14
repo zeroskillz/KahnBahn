cec94ea73d6d62f46ab4e6a5c5ef56d7456b4fc3
Ted Goodridge <zeroskillz@users.noreply.github.com>
2017-07-14 13:08:44 -0500

    oops

--- src/utils/NameToColor.js
+++ src/utils/NameToColor.js
@@ -3,10 +3,24 @@ export default function NameToColor (name) {
   for (let i = 0; i < name.length; i++) {
     hash = name.charCodeAt(i) + ((hash << 5) - hash);
   }
+
+  const hex = intToRGB(hash);
+
+
   return intToRGB(hash)
 }
 
 function intToRGB(i){
   const c = (i & 0x00FFFFFF).toString(16).toUpperCase();
   return "00000".substring(0, 6 - c.length) + c;
+}
+
+function checkLight(hex){
+  const c = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const color =  c ? {
+      r: parseInt(c[1], 16),
+      g: parseInt(c[2], 16),
+      b: parseInt(c[3], 16)
+  } : null;
+  return (0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b) < 150;
 }
